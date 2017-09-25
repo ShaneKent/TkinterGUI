a82544f68911c59518e4ab9e18b79ad9812c55de
ShaneKent <sdkent@calpoly.edu>
2017-09-24 20:42:48 -0700

    Basics of canvas drawing.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from tkinter import *
-import tkinter.messagebox
 
 root = Tk()
 
-tkinter.messagebox.showinfo("Window Title", "Shane is so cool.")
+canvas = Canvas(root, width=200, height=100)
+canvas.pack()
 
-answer = tkinter.messagebox.askquestion("Question 1", "Say hello?")
+line1 = canvas.create_line(0, 0, 200, 50)
+line2 = canvas.create_line(0, 100, 100, 25, fill="red")
 
-if answer == "yes":
-    print("Yes")
-else:
-    print("No")
+box1 = canvas.create_rectangle(20, 20, 50, 50, fill="green")
+
+canvas.delete(line2)
 
 root.mainloop()
