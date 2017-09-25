27902927b6cfd3b7c39909c7eb7fe050c80e30d7
ShaneKent <sdkent@calpoly.edu>
2017-09-24 17:04:01 -0700

    Bind events to different widget actions.

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from tkinter import *
 
 root = Tk()
 
-label_1 = Label(root, text="Name")
-label_2 = Label(root, text="Password")
-entry_1 = Entry(root)
-entry_2 = Entry(root)
+def printName():
+    print("Hello my name is Shane!")
 
-label_1.grid(row=0, sticky=E)
-label_2.grid(row=1, sticky=E)
-entry_1.grid(row=0, column=1)
-entry_2.grid(row=1, column=1)
+def printName2(event):
+    print("Hello again!")
 
-c = Checkbutton(root, text="Keep me logged in.")
-c.grid(columnspan=2)
+button_1 = Button(root, text="Print Name", command=printName)
+button_1.pack()
+
+button_2 = Button(root, text="Printt Name 2")
+button_2.bind("<Button-1>", printName2)
+button_2.pack()
 
 root.mainloop()
