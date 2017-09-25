579db662e8db7a0200e910e365198ea7531e0eec
ShaneKent <sdkent@calpoly.edu>
2017-09-24 20:37:37 -0700

    MessageBox example.

--- main.py
+++ main.py
@@ -1,39 +1,15 @@
 from tkinter import *
-
-def doNothing():
-    print("Do nothing.")
+import tkinter.messagebox
 
 root = Tk()
 
-# Main Menu
-m = Menu(root)
-root.config(menu=m)
+tkinter.messagebox.showinfo("Window Title", "Shane is so cool.")
 
-subm = Menu(m)
-m.add_cascade(label="File", menu=subm)
-subm.add_command(label="New Project", command=doNothing)
-subm.add_separator()
-subm.add_command(label="New", command=doNothing)
+answer = tkinter.messagebox.askquestion("Question 1", "Say hello?")
 
-subm2 = Menu(m)
-m.add_cascade(label="Edit", menu=subm2)
-subm2.add_command(label="Redo", command=doNothing)
-
-# Toolbar
-
-toolbar = Frame(root,  bg="blue")
-
-insertButton = Button(toolbar, text="Insert Image", command=doNothing)
-insertButton.pack(side=LEFT, padx=2, pady=2)
-
-printButton = Button(toolbar, text="Print", command=doNothing)
-printButton.pack(side=LEFT, padx=2, pady=2)
-
-toolbar.pack(side=TOP, fill=X)
-
-# Status Bar
-
-status = Label(root, text="Preparing to do nothing...", bd=1, relief=SUNKEN, anchor=W)
-status.pack(side=BOTTOM, fill=X)
+if answer == "yes":
+    print("Yes")
+else:
+    print("No")
 
 root.mainloop()
