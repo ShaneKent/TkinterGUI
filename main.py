0452d46a84060da9bd59e64499a23390f6283a76
ShaneKent <sdkent@calpoly.edu>
2017-09-24 20:33:49 -0700

    Realistic GUI operation. Menu bar, toolbar, and status bar.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def doNothing():
 
 root = Tk()
 
+# Main Menu
 m = Menu(root)
 root.config(menu=m)
 
@@ -17,5 +18,22 @@ subm.add_command(label="New", command=doNothing)
 subm2 = Menu(m)
 m.add_cascade(label="Edit", menu=subm2)
 subm2.add_command(label="Redo", command=doNothing)
+
+# Toolbar
+
+toolbar = Frame(root,  bg="blue")
+
+insertButton = Button(toolbar, text="Insert Image", command=doNothing)
+insertButton.pack(side=LEFT, padx=2, pady=2)
+
+printButton = Button(toolbar, text="Print", command=doNothing)
+printButton.pack(side=LEFT, padx=2, pady=2)
+
+toolbar.pack(side=TOP, fill=X)
+
+# Status Bar
+
+status = Label(root, text="Preparing to do nothing...", bd=1, relief=SUNKEN, anchor=W)
+status.pack(side=BOTTOM, fill=X)
 
 root.mainloop()
