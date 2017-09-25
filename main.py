3c87e70845dcf2b29ab3abe1cee69049b2f54aef
ShaneKent <sdkent@calpoly.edu>
2017-09-24 21:30:01 -0700

    Basic app for incrementing and decrementing counter.

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 from tkinter import *
 
+class App:
+    def __init__(self, master):
+        self.counter = 0
+        self.master = master
+        
+        self.master.minsize(width=200, height=200)
+        self.master.maxsize(width=200, height=200)
+
+    def createText(self):
+        self.counter_text = Label(self.master, text="0")
+        self.counter_text.grid(row=0, column=1)
+
+    def createButtons(self):
+        self.inc_button = Button(self.master, text="Increase", command=self.incCounter)
+        self.dec_button = Button(self.master, text="Decrease", command=self.decCounter)
+
+        self.inc_button.grid(row=0, column=0)
+        self.dec_button.grid(row=0, column=2)
+
+    def incCounter(self):
+        self.counter += 1
+        self.counter_text.config(text=str(self.counter))
+
+    def decCounter(self):
+        self.counter -= 1
+        self.counter_text.config(text=str(self.counter))
+
 root = Tk()
 
-canvas = Canvas(root, width=200, height=100)
-canvas.pack()
-
-line1 = canvas.create_line(0, 0, 200, 50)
-line2 = canvas.create_line(0, 100, 100, 25, fill="red")
-
-box1 = canvas.create_rectangle(20, 20, 50, 50, fill="green")
-
-canvas.delete(line2)
+app = App(root)
+app.createText()
+app.createButtons()
 
 root.mainloop()
