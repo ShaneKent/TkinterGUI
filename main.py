1dde160d055ac0d7dd23c540f1d563d45a0804c2
ShaneKent <sdkent@calpoly.edu>
2017-09-24 17:16:50 -0700

    Basic class structure that can be used in Tkinter.

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 from tkinter import *
 
+class ShanesButtons:
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.printButton = Button(frame, text="Print Message", command=self.printMessage)
+        self.printButton.pack(side=LEFT)
+
+        self.quitButton = Button(frame, text="Quit", command=frame.quit)
+        self.quitButton.pack(side=LEFT)
+
+    def printMessage(self):
+        print("Wow!")
+
 root = Tk()
-
-def leftClick(event):
-    print("Left")
-
-def rightClick(event):
-    print("Right")
-
-def middleClick(event):
-    print("Middle")
-
-frame = Frame(root, width=300, height=250)
-frame.bind("<Button-1>", leftClick)
-frame.bind("<Button-2>", rightClick)
-frame.bind("<Button-3>", middleClick)
-frame.pack()
-
+s = ShanesButtons(root)
 root.mainloop()
